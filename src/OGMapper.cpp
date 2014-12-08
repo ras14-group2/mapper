93629af100d1b301cacc7cb4d6a835589fdca071
mgeppert <geppert@kth.se>
2014-12-08 13:51:20 +0100

    commented error generating line in findPath to enable building

--- src/OGMapper.cpp
+++ src/OGMapper.cpp
@@ -306,7 +306,7 @@ bool OGMapper::findPath(mapper::PathToObject::Request &req, mapper::PathToObject
 	
 	//Return some dummy path
 	res.path[0] = req.start;
-	res.length = 1;
+//	res.length = 1;
 	
 	return true;
 }
